ac9bdd79a7e55b1fbf63087ccb8a8ec4ccf96a72
Richard Spitkovsky <rjspitkovsky@gmail.com>
2017-12-21 19:53:08 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,8 @@
 def valid_move?(board, index)
   if index > -1 && index < 9
      true
-  elsif position_taken?(board, index) == false
-     nil 
+  if position_taken?(board, index) == true
+     false
 end
 end
 
